021e288d64cd2183f06f2972313311634f7fa7b1
Carla <113267384+RuixDsgn@users.noreply.github.com>
2023-06-16 13:47:01 -0700

    complete lab with all tests passing

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [num for num in num_list if num % 2 == 0]
+    if not evens:
+        return []
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_sentences = [sentence + "!" for sentence in sentence_list]
+    if not exclamation_sentences:
+        return []
+    return exclamation_sentences
